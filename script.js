f1b898d73e1675ce78700e41bb617537e89a51d1
Riley Powers <wzrd1111@gmail.com>
2020-02-25 21:46:49 -0800

    https://github.com/rileypowers/QUIZ-APP1

---
+++ script.js
@@ -0,0 +1,60 @@
+// const QUESTIONS = [
+//   {
+//     question: 'What is mise-en-place?',
+//     answers: {
+//       A: 'A traditional French country dish',
+//       B: 'The technique of rendering animal fat using low, slow heat',
+//       C: 'A city in France',
+//       D: 'A phrase which means “to put everything in its place”'
+//     },
+//     CORRECT: 'Mise-en-place is a French term. Directly translated, it means “to put everything in its place”, and it is implemented by preparing all ingredients before one begins cooking: veg chopped, items that need to be brought to room temp done thusly, all ingredients measured and laid out.',
+//     INCORRECT: 'It is not *autopopulate with wrong answer that was clicked*. Keep going!'
+//   },
+
+//   {
+//     question: 'What is another term for a sourdough starter?',
+//     answers: {
+//       A: 'Bark',
+//       B: 'Sponge',
+//       C: 'Colony',
+//       D: 'Fougasse',
+//     },
+//     CORRECT: 'Back when we were still colonizing the US, settlers would carry around sourdough starter that could last their lifetime. The common term for this then was a “sponge.” Good job.',
+//     INCORRECT: 'It is not called *autopopulate with wrong answer that was clicked*. Better luck next time!',
+//   },
+
+//   {
+//     question: 'Which of these is not a pasta shape?',
+//     answers: {
+//       A:  'Bucatini',
+//       B:  'Culurgiones',
+//       C:  'Bisollini',
+//       D:	'Ditalini',
+//     },
+//     CORRECT: 'Bucatini is like thick spaghetti with a hole in the center to trap sauce. Culurgiones is a pleated dumpling-like pasta typically filled with potato and mint. Ditalini is tiny star-shaed pasta usually used for soup. Bisollini is a made up word.',
+//     INCORRECT: 'It is not *autopopulate with wrong answer that was clicked*. Keep going.',
+//   },
+
+//   {
+//     question: 'Which oil should not be used for deep frying?',
+//     answers: {
+//       A:  'Olive Oil',
+//       B:	'Canola Oil',
+//       C:	'Peanut Oil',
+//       D:	'Avocado Oil',
+//     },
+//     CORRECT: 'Olive oil has a much lower smoke point than the other oils listed. It has small organic material that lend taste, but will burn and turn bitter and the temps required for deep frying.',
+//     INCORRECT: '*autopopulate with wrong answer that was clicked* is fine for deep frying. Try the final question next.',
+//   },
+
+//   {
+//     question:  'What is Adobo?',
+//     answers: {
+//       A:'A traditional Philippine dish',
+//       B:	'A Spanish sauce ',
+//       C:	'A Caribbean seasoning',
+//       D:	'All of the above',
+//     },
+//     CORRECT: 'Adobo is a term that transcends cultures. In Filipino culture, it is a cooking technique and therefor the name of a dish. It is also a Spanish sauce you may have seen at the grocery store, canned with chilis. In the Caribbean, adobo is a seasoning of mostly salt, garlic podwer, and MSG.',
+//     INCORRECT: 'You are not wrong, but the correct answer is all of the above. This term transcends cultures.',
+//   },]
